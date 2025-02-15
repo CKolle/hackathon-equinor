233c5ed9f060b43667df33c688ee19a0f468da55
Jon Henrik <piehunter123@gmail.com>
2025-02-15 14:47:54 +0100

    Update cell.js

--- game/js/core/cell.js
+++ game/js/core/cell.js
@@ -1,13 +1,15 @@
 class Cell {
-    constructor(x, y, acceptsElectricity = true){
+    constructor(x, y, acceptsElectricity = false){
         this.x = x;
         this.y = y;
         this.acceptsElectricity = acceptsElectricity;
         this.electricityLevel = 0;
+        this.type = cells.EMPTY;
     }
 }
 
 const cells = Object.freeze({
+    EMPTY: "Empty",
     WINDMILL: "Windmill",
     CABLE: "Cable",
     CITY: "City",
