2965aa086e47eb90145006a34ac59043e1583614
Christopher Liebich Kolle <cl.kolle@stud.uis.no>
2025-02-15 20:50:53 +0100

    Fixes rendering in populate shop panel

--- game/js/ui/populateShopPanel.js
+++ game/js/ui/populateShopPanel.js
@@ -1,37 +1,66 @@
-function populateShopPanel(){
+function populateShopPanel() {
     let buildableStructures = [];
     Object.keys(config["cells"]).forEach(cellType => {
-        if(config.cells[cellType].buildCost){
+        if (config.cells[cellType].buildCost) {
             buildableStructures.push(cellType);
         }
     });
     let shopDiv = document.getElementById("shop");
-    
+
+    shopDiv.style.display = 'grid';
+    shopDiv.style.gridTemplateColumns = 'repeat(3, 1fr)';
+    shopDiv.style.gap = '4px';
+    shopDiv.style.padding = '4px';
+
+    const shopWidth = shopDiv.clientWidth;
+    // 8px padding on each side
+    const displaySize = Math.floor((shopWidth - (8 + 8)) / 3);
+
     buildableStructures.forEach(cellType => {
         let img = new Image();
         img.src = "../assets/tileset.png";
         img.onload = function () {
             let canvas = document.createElement("canvas");
-            canvas.addEventListener("click",(e)=>{
+            canvas.addEventListener("click", (e) => {
                 clickBuildStructure(cellType);
                 e.preventDefault();
             });
-            let ctx = canvas.getContext("2d");
 
-            // Set canvas size to only show the first 32 pixels
-            canvas.width = 32;
-            canvas.height = 32;
+            const pixelRatio = window.devicePixelRatio || 1;
+            canvas.width = displaySize * pixelRatio;
+            canvas.height = displaySize * pixelRatio;
 
-            // Draw only the first 32 pixels of the image
-            bounds = config.cells[cellType].tiles.default.bounds;
-            console.log(bounds[0]*16,bounds[1]*16);
-            ctx.drawImage(img, 16*bounds[0], 16*bounds[1], 32, 32, 0, 0, 32, 32);
+            canvas.style.width = `${displaySize}px`;
+            canvas.style.height = `${displaySize}px`;
+            canvas.style.display = 'block';
+            canvas.style.margin = '0';
+
+            let ctx = canvas.getContext("2d", {
+                alpha: true,
+                antialias: false
+            });
+
+            ctx.scale(pixelRatio, pixelRatio);
+            ctx.imageSmoothingEnabled = false;
+
+            const bounds = config.cells[cellType].tiles.default.bounds;
+            ctx.drawImage(
+                img,
+                bounds[0] * 16,
+                bounds[1] * 16,
+                32,
+                32,
+                0,
+                0,
+                displaySize,
+                displaySize
+            );
 
             shopDiv.appendChild(canvas);
         };
     });
 }
 
-let clickBuildStructure = (type)=>{
+let clickBuildStructure = (type) => {
     console.log(type);
 };
