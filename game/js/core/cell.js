c3f02f44ed194459cbb1a984db244396e0e23c2f
Jon Henrik <piehunter123@gmail.com>
2025-02-15 14:26:02 +0100

    Electricity diffusion system

---
+++ game/js/core/cell.js
@@ -0,0 +1,8 @@
+class Cell {
+    constructor(x, y, acceptsElectricity = true){
+        this.x = x;
+        this.y = y;
+        this.acceptsElectricity = acceptsElectricity;
+        this.electricityLevel = 0;
+    }
+}
---
+++ game/js/systems/electricity.js
@@ -0,0 +1,22 @@
+class ElectricitySystem {
+    constructor(grid){
+        this.activeCells = []; // Cables
+        this.grid = grid;
+    }
+
+    addTileObject(obj){
+        this.activeCells.push(obj);
+    }
+
+    update(gameState, dt){
+        this.activeCells.forEach(cable => {
+            // Check for adjacent tiles if we can share electricity with an object
+            grid.findConnectedactiveCells(cable.x, cable.y).forEach(neighbor => {
+                if(!neighbor.acceptsElectricity) return;
+                let donation = cable.electricityLevel * 0.5;
+                neighbor.electricityLevel += donation;
+                cable.electricityLevel -= donation;
+            });
+        });
+    }
+}
